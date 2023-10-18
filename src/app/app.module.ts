4533dcaee41fd9a643e607e3f2cad1d3653dd8b1
Sparsh Handa <104482831+sparsh-handa@users.noreply.github.com>
2023-10-18 11:01:46 +0530

    updated

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BookSearchComponent } from './book-search/book-search.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookSearchComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
